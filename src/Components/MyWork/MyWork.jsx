dfa9b8ed1ac5ce0a4011db4009000ca92bf77522
Harikesh312 <kashyapharikesh412@gmail.com>
2025-06-04 10:29:10 +0530

    debug

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import './MyWork.css'
-import MyWorkData from '../../assets/MyWorkData'
-import { FaArrowRightLong } from 'react-icons/fa6'
+import React from "react";
+import "./MyWork.css";
+import MyWorkData from "../../assets/MyWorkData";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 function MyWork() {
   return (
-    <div id='work' className='mywork'>
+    <div id="work" className="mywork">
       <div className="mywork-title">
         <h1>My Latest Work</h1>
       </div>
       <div className="mywork-container">
-        {MyWorkData.map((work,index)=>{
-            return <a href={work.w_link} key={index}><img src={work.w_img} alt="" /></a>
+        {MyWorkData.map((work, index) => {
+          return (
+            <a href={work.w_link} key={index}>
+              <img src={work.w_img} alt="" />
+            </a>
+          );
         })}
       </div>
       <div className="mywork-showmore">
-        Show more
-        <FaArrowRightLong />
+        <a href="https://github.com/Harikesh312">
+          Show more
+          <FaArrowRightLong />
+        </a>
       </div>
     </div>
-  )
+  );
 }
 
-export default MyWork
+export default MyWork;
